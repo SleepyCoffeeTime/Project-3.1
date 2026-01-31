95a1c04ceadaa5b53b88dcfa61f8e2452736324a
SleepyCoffeeTime <mercedthalia@gmail.com>
2026-01-30 23:20:01 -0700

    added beginning code for front end

--- Inventoryapp/frontend/src/components/ItemCard.jsx
+++ Inventoryapp/frontend/src/components/ItemCard.jsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getAllItems } from '../services/api';
+import { useNavigate } from 'react-router-dom';
 
-export default function ItemCard(){
-    //return
+export default function ItemsPage() {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
 }
